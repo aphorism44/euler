9918ac71b32cb4b3135da085ddfc66633c4e99e3
aphorism44 <dom.jesse6@gmail.com>
2019-03-16 13:16:02 -0500

    solved 35

--- euler/Euler35.java
+++ euler/Euler35.java
@@ -10,53 +10,43 @@ The number, 197, is called a circular prime because all rotations of the digits:
 There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
 
 How many circular primes are there below one million?
- * 
+ 
+NOTE - this solves it, but takes about 30 seconds...
+  
  */
 
 public class Euler35 {
 	
 	public static void main(String[] args) {
 		
-		ArrayList<Integer> ints = new ArrayList<Integer>();
-		ints.add(1);
-		ints.add(2);
-		ints.add(3);
+		//long startTime = System.currentTimeMillis();
 		
-		for (int i: ints)
-			System.out.println(i);
-		
-		ints = combinatorics.getIntPermutations(ints);
-		
-		for (int i: ints)
-			System.out.println(i);
-		
-		/*
-		 * 
-		ArrayList<Integer> primes = sieveEratosthenes.getPrimes(100);
-		ArrayList<Integer> circularPrimes = new ArrayList<Integer>();
+		ArrayList<Integer> primes = sieveEratosthenes.getPrimes(1000000);
+		//ArrayList<Integer> circularPrimes = new ArrayList<Integer>(); //testing only
 		int numCircularPrimes = 0;
 		
 		for (int i: primes) {
-			ArrayList<Integer> digits = new ArrayList<Integer>();
-			int temp = i;
-			while (temp > 0) {
-				digits.add(temp % 10);
-				temp /= 10;
+			ArrayList<Integer> rotations = combinatorics.getIntRotations(i);
+			boolean isCircularPrime = true;
+			for (int r: rotations) {
+				if (!primes.contains(r)) {
+					isCircularPrime = false;
+					break;
+				}
+			}
+			if (isCircularPrime) {
+				//circularPrimes.add(i);
+				numCircularPrimes++;
 			}
 			
-			if (digits.size() == 1) {
-				circularPrimes.add(i);
-				numCircularPrimes++;
-			} else {
-				//perform permutations
-				ArrayList<Integer> permutations = new ArrayList<Integer>();
-				for (int index = 0; index < digits.size(); index++) {
-					int firstInt = 
-				}
-				
-			}
 		}
-		*/
+		
+		System.out.println("Circular primes: " + numCircularPrimes);
+		//for (int n: circularPrimes)
+			//System.out.println(n);
+		
+		//long endTime = System.currentTimeMillis();
+		//System.out.println("That took " + (endTime - startTime) / 1000 + " seconds");
 		
 	}
 	
--- helpers/combinatorics.java
+++ helpers/combinatorics.java
@@ -4,26 +4,56 @@ import java.util.ArrayList;
 
 public class combinatorics {
 
-	public static ArrayList<Integer> getIntPermutations(ArrayList<Integer> arr) {
+	
+	//takes in a number and returns an array of all possible permutations of its digits
+	public static ArrayList<Integer> getIntPermutations(int num) {
+		ArrayList<Character> numArr = new ArrayList<Character>();
+		for (char c: Integer.toString(num).toCharArray())
+			numArr.add(c);
 		ArrayList<Integer> permutations = new ArrayList<Integer>();
-		//base case
-		if (arr.size() == 1) {
-			return arr;
-		} else if (arr.size() > 1) {
+		//base case 1
+		if (numArr.size() < 2) {
+			permutations.add(num);
+			return permutations;
+		//base case 2
+		} else if (numArr.size() == 2) {
+			String sNum1 = new StringBuilder().append(numArr.get(0)).append(numArr.get(1)).toString();
+			String sNum2 = new StringBuilder().append(numArr.get(1)).append(numArr.get(0)).toString();
+			permutations.add(Integer.valueOf(sNum1));
+			permutations.add(Integer.valueOf(sNum2));
+			return permutations;
+		} else {
 			//recursive relation
-			for (int i = 0; i < arr.size(); i++) {
-				ArrayList<Integer> tempPerm = new ArrayList<Integer>(permutations);
-				String front = tempPerm.remove(i).toString();
-				ArrayList<Integer> newPerms = getIntPermutations(tempPerm);
+			for (int i = 0; i < numArr.size(); i++) {
+				ArrayList<Character> tempNumArr = new ArrayList<Character>(numArr);
+				char currNumStr = tempNumArr.remove(i);
+				StringBuilder tempNumStr = new StringBuilder();
+				for (char c: tempNumArr)
+					tempNumStr.append(c);
+				ArrayList<Integer> newPerms = getIntPermutations(Integer.valueOf(tempNumStr.toString()));
 				for (int p: newPerms) {
-					permutations.add(Integer.valueOf(front + p));
+					String newNum = currNumStr + String.valueOf(p);
+					permutations.add(Integer.valueOf(newNum));
 				}
 			}
-			
-			
+			return permutations;
 		}
-		
-		return permutations;
 	}
 	
+	//takes in a number and returns an array of all possible rotations of its digits
+	//(a rotation of 197 = {197, 971, 719}
+	public static ArrayList<Integer> getIntRotations(int num) {
+		ArrayList<Integer> rotations = new ArrayList<Integer>();
+		String numStr = String.valueOf(num);
+		int numLength = numStr.length();
+		for (int i = 0; i < numLength; i++) {
+			StringBuilder numBld = new StringBuilder().append(numStr.charAt(i));
+			for (int j = i + 1; j < numLength + i; j++) {
+				numBld.append(numStr.charAt(j % numLength));
+			}
+			rotations.add(Integer.valueOf(numBld.toString()));
+		}
+		
+		return rotations;
+	}
 }
